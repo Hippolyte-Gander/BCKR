f4ced4f620e9795313b7bb595441e3b9e3bfc424
Hippolyte G <hippolyte.gander5@gmail.com>
2024-08-25 21:43:36 +0200

    début de fonction ajout commentaire à événement

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CommentaireController extends AbstractController
 {
-    #[Route('/commentaire', name: 'app_commentaire')]
-    public function index(): Response
+    // #[Route('/commentaire', name: 'app_commentaire')]
+    // public function index(): Response
+    // {
+    //     return $this->render('commentaire/index.html.twig', [
+    //         'controller_name' => 'CommentaireController',
+    //     ]);
+    // }
+
+
+    // ------------- FORMULAIRE CREATION NOUVEAU COMMENTAIRE ------------- fonction à compléter
+    #[Route('/evenement/{id}', name: 'show_evenement')]
+    #[Route('/commentaire/new', name: 'new_commentaire')]
+    public function new_edit(Commentaire $commentaire = null, Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('commentaire/index.html.twig', [
-            'controller_name' => 'CommentaireController',
+        if (!$commentaire) {
+            $commentaire = new Commentaire();
+        }
+
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $commentaire = $form->getData();
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_commentaire');
+        }
+        
+        return $this->render('commentaire/new.html.twig',[
+            'formAddCommentaire'=> $form,
+            'edit'=> $commentaire->getId()
         ]);
     }
 }
---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Evenement;
+use App\Entity\Commentaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('message', TextareaType::class, [
+                'label'=>"Commentez ici",
+                'required'=> true,
+                'attr'=> [
+                    'class'=> 'texte-formulaire'
+                ]
+                ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
