112f788a9ed166922e9798ae7066372490c9e9ff
Hippolyte Gander <hippolyte.gander5@gmail.com>
2024-09-25 13:53:48 +0200

    idem ajout carrousel qui marche pas

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'e3321c9037980ee9dbfe031d76610dacb3bab594',
+        'reference' => 'd8a0a57131d3cb296a17e9389ed1d6a2911585e7',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'e3321c9037980ee9dbfe031d76610dacb3bab594',
+            'reference' => 'd8a0a57131d3cb296a17e9389ed1d6a2911585e7',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
