f49fb52e6d79aa56dcd356b54d4e50930f62ab7c
Hippolyte Gander <hippolyte.gander5@gmail.com>
2024-10-24 12:01:43 +0200

    - edit lien des cards - correction affichage flash sur page login + pour tous les types de messages

--- assets/js/notif.js
+++ assets/js/notif.js
@@ -1,7 +1,17 @@
 function hideMsg() {
-    let elements = document.getElementsByClassName('alert');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.visibility = "hidden";
+    let alerts = document.getElementsByClassName('alert');
+    for (let i = 0; i < alerts.length; i++) {
+        alerts[i].style.visibility = "hidden";
+    }
+    
+    let successes = document.getElementsByClassName('flash-success');
+    for (let i = 0; i < successes.length; i++) {
+        successes[i].style.visibility = "hidden";
+    }
+    
+    let dangers = document.getElementsByClassName('flash-danger');
+    for (let i = 0; i < dangers.length; i++) {
+        dangers[i].style.visibility = "hidden";
     }
 }
 
