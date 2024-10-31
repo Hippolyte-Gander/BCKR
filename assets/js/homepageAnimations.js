a69508516d27f90fd0ac467b47be3d1d8be82a1d
Hippolyte G <hippolyte.gander5@gmail.com>
2024-10-31 16:22:54 +0100

    ajout effet apparition - page club - homepage (uniquement 1ère section)

--- assets/js/homepageAnimations.js
+++ assets/js/homepageAnimations.js
@@ -6,18 +6,42 @@
 //     });
 // });
 
-// ajouter la classe 'show' à tous les éléments visibles à l'écran et l'enlève lorsqu'ils n'y sont plus
-// ajoute la classe 'shown' aux éléments pour qu'il n'apparaissent qu'une fois
+// ------------------ Fonction pour gérer les classes 'hidden' et 'show' ------------------
+
+// on sélectionne les éléments avec la classe 'hidden'
+const hiddenElements = document.querySelectorAll('.hidden');
+// on sélectionne les éléments avec la classe 'hidden-once'
+const hiddenOnceElements = document.querySelectorAll('.hidden-once');
+
+// on crée un nouvel observateur pour détecter la visibilité
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
+        const element = entry.target;
+        
+        // si l'élément est visible
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            entry.target.classList.add('shown');
+            // si l'élément a la classe 'hidden-once'
+            if (element.classList.contains('hidden-once')) {
+                // on ajoute la classe 'show'
+                element.classList.add('show');
+                // on arrête d'observer l'élément
+                observer.unobserve(element); 
+            }
+            // si l'élément a la classe 'hidden'
+            else if (element.classList.contains('hidden')) {
+                // ajoute la classe 'show'
+                element.classList.add('show');
+            }
         } else {
-            entry.target.classList.remove('show');
+            // si l'élément a la classe 'hidden'
+            if (element.classList.contains('hidden')) {
+                // retire la classe 'show' (quand n'est plus observé)
+                element.classList.remove('show');
+            }
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+// on observe tous les éléments 'hidden' et 'hidden-once'
 hiddenElements.forEach((element) => observer.observe(element));
+hiddenOnceElements.forEach((element) => observer.observe(element));
